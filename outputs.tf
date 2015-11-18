3870e362c61180cf755d961bc3e7cb891f2c7182
Hector Castro <hectcastro@gmail.com>
2015-11-17 22:37:37 -0500

    Apply hclfmt
    See: https://github.com/fatih/hclfmt

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,15 @@
 output "id" {
   value = "${aws_vpc.default.id}"
 }
+
 output "public_subnet_ids" {
   value = "${join(",", aws_subnet.public.*.id)}"
 }
+
 output "private_subnet_ids" {
   value = "${join(",", aws_subnet.private.*.id)}"
 }
+
 output "bastion_hostname" {
   value = "${aws_instance.bastion.public_dns}"
 }
