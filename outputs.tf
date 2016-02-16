8861bfe676538ea64469cb2e4400c40580313750
Hector Castro <hcastro@azavea.com>
2016-02-16 16:32:55 -0500

    Add support for AWS NAT gateway
    Replaces the existing NAT instances with support for AWS managed NAT
    gateways.
    
    Attempts to resolve #5

--- outputs.tf
+++ outputs.tf
@@ -14,10 +14,6 @@ output "bastion_hostname" {
   value = "${aws_instance.bastion.public_dns}"
 }
 
-output "nat_security_group_id" {
-  value = "${aws_security_group.nat.id}"
-}
-
 output "cidr_block" {
   value = "${var.cidr_block}"
 }
