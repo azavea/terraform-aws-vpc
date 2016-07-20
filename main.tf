02649c65a34549b929ef2b9bc0f851f9af042858
Hector Castro <hcastro@azavea.com>
2016-07-20 11:16:54 -0400

    Remove all security group rules
    Previously, this module setup a handful of security group rules for the
    VPC bastion security group. Now that we emit the bastion's security
    group ID as module output, it is possible for the module consumer to add
    security group rules externally.

--- main.tf
+++ main.tf
@@ -132,42 +132,6 @@ resource "aws_security_group" "bastion" {
   }
 }
 
-resource "aws_security_group_rule" "bastion_ssh_ingress" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["${var.external_access_cidr_block}"]
-  security_group_id = "${aws_security_group.bastion.id}"
-}
-
-resource "aws_security_group_rule" "bastion_ssh_egress" {
-  type              = "egress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.bastion.id}"
-}
-
-resource "aws_security_group_rule" "bastion_http_egress" {
-  type              = "egress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.bastion.id}"
-}
-
-resource "aws_security_group_rule" "bastion_https_egress" {
-  type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = "${aws_security_group.bastion.id}"
-}
-
 resource "aws_instance" "bastion" {
   ami                         = "${var.bastion_ami}"
   availability_zone           = "${element(split(",", var.availability_zones), 0)}"
