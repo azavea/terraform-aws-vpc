7db1576b455dcb1277b21dc85606a4b21fe8c691
Hector Castro <hcastro@azavea.com>
2016-08-29 10:15:46 -0400

    Add Terraform 0.7 support
    Convert all comma-delimited string for subnets and availability zones to
    lists.
    
    See: https://www.terraform.io/upgrade-guides/0-7.html

--- variables.tf
+++ variables.tf
@@ -25,15 +25,18 @@ variable "external_access_cidr_block" {
 }
 
 variable "public_subnet_cidr_blocks" {
-  default = "10.0.0.0/24,10.0.2.0/24"
+  type    = "list"
+  default = ["10.0.0.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidr_blocks" {
-  default = "10.0.1.0/24,10.0.3.0/24"
+  type    = "list"
+  default = ["10.0.1.0/24", "10.0.3.0/24"]
 }
 
 variable "availability_zones" {
-  default = "us-east-1a,us-east-1b"
+  type    = "list"
+  default = ["us-east-1a", "us-east-1b"]
 }
 
 variable "bastion_ami" {}
