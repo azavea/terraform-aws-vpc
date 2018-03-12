984952490aac8bc6bb4d99569a24fc3632c5527f
Taylor Nation <tnation14@gmail.com>
2018-03-12 08:11:50 -0400

    terraform fmt

--- main.tf
+++ main.tf
@@ -137,7 +137,7 @@ resource "aws_security_group" "bastion" {
 }
 
 resource "aws_network_interface_sg_attachment" "bastion" {
-  security_group_id = "${aws_security_group.bastion.id}"
+  security_group_id    = "${aws_security_group.bastion.id}"
   network_interface_id = "${aws_instance.bastion.primary_network_interface_id}"
 }
 
