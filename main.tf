1e63a1ff7b3caf7afc891f3683b073599745ac30
Taylor <tnation14@gmail.com>
2018-03-15 10:34:00 -0400

    Give option to add bastion to multiple security groups
    * Give option to add bastion to multiple security groups
    
    Attach bastion to security group with `aws_network_interface_sg_attachment`
    and output Bastion primary network interface id. This allows users to
    attach the bastion to multiple security groups after creation.

--- main.tf
+++ main.tf
@@ -136,13 +136,17 @@ resource "aws_security_group" "bastion" {
   }
 }
 
+resource "aws_network_interface_sg_attachment" "bastion" {
+  security_group_id    = "${aws_security_group.bastion.id}"
+  network_interface_id = "${aws_instance.bastion.primary_network_interface_id}"
+}
+
 resource "aws_instance" "bastion" {
   ami                         = "${var.bastion_ami}"
   availability_zone           = "${element(var.availability_zones, 0)}"
   instance_type               = "${var.bastion_instance_type}"
   key_name                    = "${var.key_name}"
   monitoring                  = true
-  vpc_security_group_ids      = ["${aws_security_group.bastion.id}"]
   subnet_id                   = "${element(aws_subnet.public.*.id, 0)}"
   associate_public_ip_address = true
 
