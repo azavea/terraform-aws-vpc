4e31487d43f98bd5b8b4370b2529fd346756b7c1
Hector Castro <hectcastro@gmail.com>
2019-12-21 22:59:29 -0500

    Add tags to aws_internet_gateway

--- main.tf
+++ main.tf
@@ -18,6 +18,15 @@ resource "aws_vpc" "default" {
 
 resource "aws_internet_gateway" "default" {
   vpc_id = aws_vpc.default.id
+
+  tags = merge(
+    {
+      Name        = "gwInternet",
+      Project     = var.project,
+      Environment = var.environment
+    },
+    var.tags
+  )
 }
 
 resource "aws_route_table" "private" {
