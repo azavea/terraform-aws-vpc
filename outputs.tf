53efcf0b9f7fbbc517bd80b8584f617a39ca84c7
Hector Castro <43639+hectcastro@users.noreply.github.com>
2019-12-21 23:04:29 -0500

    Upgrade module to support Terraform 0.12 (#24)
    Following the instructions outlined in the Terraform documentation, update the module's configuration to support Terraform 0.12. Despite containing minor changes, this is expected to cause a major version bump, because the changes are not backwards compatible.

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,39 @@
 output "id" {
-  value = "${aws_vpc.default.id}"
+  value       = aws_vpc.default.id
+  description = "VPC ID"
 }
 
 output "public_subnet_ids" {
-  value = ["${aws_subnet.public.*.id}"]
+  value       = aws_subnet.public.*.id
+  description = "List of public subnet IDs"
 }
 
 output "private_subnet_ids" {
-  value = ["${aws_subnet.private.*.id}"]
+  value       = aws_subnet.private.*.id
+  description = "List of private subnet IDs"
 }
 
 output "bastion_hostname" {
-  value = "${aws_instance.bastion.public_dns}"
+  value       = aws_instance.bastion.public_dns
+  description = "Public DNS name for bastion instance"
 }
 
 output "bastion_security_group_id" {
-  value = "${aws_security_group.bastion.id}"
+  value       = aws_security_group.bastion.id
+  description = "Security group ID tied to bastion instance"
 }
 
 output "bastion_network_interface_id" {
-  value = "${aws_instance.bastion.primary_network_interface_id}"
+  value       = aws_instance.bastion.primary_network_interface_id
+  description = "Elastic Network Interface (ENI) ID of the Bastion instance's primary network interface"
 }
 
 output "cidr_block" {
-  value = "${var.cidr_block}"
+  value       = var.cidr_block
+  description = "The CIDR block associated with the VPC"
 }
 
 output "nat_gateway_ips" {
-  value = ["${aws_eip.nat.*.public_ip}"]
+  value       = aws_eip.nat.*.public_ip
+  description = "List of Elastic IPs associated with NAT gateways"
 }
